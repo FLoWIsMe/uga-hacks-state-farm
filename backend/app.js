4b039543b17e64bd4fee7c3cf6225ce13395cdac
jcook <epicdevelopers26@gmail.com>
2024-02-10 14:21:31 -0500

    backened

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
 // app.js
 const express = require('express');
 var cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+//app.get("/", (req, res) => {
+//  res.sendFile(path.join(__dirname, "public", "index.html"));
+ //});
+ 
+ app.use(express.static("../frontend/public"));
+
 
 // Start the server
 app.listen(port, () => {
