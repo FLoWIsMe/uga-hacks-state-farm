6ad9890a59aecb7888caf679902be55a20789ca2
Daelyn Doughty <87499728+FLoWIsMe@users.noreply.github.com>
2024-02-10 22:51:26 -0500

    Add files via upload

---
+++ Breakdown.jsx
@@ -0,0 +1,92 @@
+import { Stack, Text, Box, Button } from '@chakra-ui/react'
+
+
+export default function Breakdown() {
+
+  return(
+   <Stack
+     width="100vw"
+     height="100vh"
+     maxWidth="100%"
+     background="#5F5F5F"
+     boxShadow="0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+   >
+     <Stack width="300px" height="900px" maxWidth="100%" background="#262626">
+       <Stack width="300px" height="91px" maxWidth="100%" background="#FFBCDC">
+         <Text
+           fontFamily="Inter"
+           fontWeight="semibold"
+           fontSize="32px"
+           color="#262626"
+           width="164px"
+           height="33px"
+           textAlign="center"
+         >
+           Allocation
+         </Text>
+         <Stack width="32px" height="29px" />
+       </Stack>
+       <Stack width="300px" height="91px" maxWidth="100%" background="#FFBCDC">
+         <Text
+           fontFamily="Inter"
+           fontWeight="semibold"
+           fontSize="32px"
+           color="#262626"
+           width="164px"
+           height="33px"
+           textAlign="center"
+         >
+           Accounts
+         </Text>
+         <Stack width="32px" height="32px" />
+       </Stack>
+       <Stack width="300px" height="91px" maxWidth="100%" background="#FFBCDC">
+         <Text
+           fontFamily="Inter"
+           fontWeight="semibold"
+           fontSize="32px"
+           color="#262626"
+           width="164px"
+           height="33px"
+           textAlign="center"
+         >
+           Profile
+         </Text>
+         <Stack width="32px" height="32px" />
+       </Stack>
+       <Stack width="32px" height="32px" />
+     </Stack>
+     <Stack width="254px" height="96px" maxWidth="100%">
+       <Box width="254px" height="96px" maxWidth="100%" />
+     </Stack>
+     <Button>
+       <Box
+         borderRadius="80px"
+         width="508px"
+         height="121px"
+         maxWidth="100%"
+         background="#FCBCDC"
+       />
+       <Text
+         fontFamily="Inter"
+         fontWeight="semibold"
+         fontSize="32px"
+         color="#262626"
+         width="378.98px"
+         height="51.54px"
+         maxWidth="100%"
+         textAlign="center"
+       >
+         See your potential
+       </Text>
+       <Stack width="30px" height="27px" />
+     </Button>
+     <span className="unsupported" />
+     <span className="unsupported" />
+     <span className="unsupported" />
+     <span className="unsupported" />
+     <span className="unsupported" />
+     <span className="unsupported" />
+   </Stack>
+ )
+}
